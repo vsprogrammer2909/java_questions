f08f5df039d059c362ed3a53cd4d44a89daf4876
Vindya shivam <vindyashivam2909@gmail.com>
2021-10-01 20:11:20 +0530

    1.1

--- fp1.1.java
+++ fp1.1.java
@@ -3,5 +3,9 @@ class Pattern
     public static void main(String args[])
     {
         System.out.println("Hello world");
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("Hello world five times LOL");
+        }
     }
 }
